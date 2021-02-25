5ed8e99f9c7fd81f01b51ca0831198fc55374e3b
syamo1111 <mtsza.mtznh@gmail.com>
2021-02-25 22:48:46 +0900

    chapter3 add

---
+++ Chapter3/Janken/Player.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Janken
+{
+    public class Player
+    {
+        public const int STONE = 0;
+        public const int SCISSORS = 1;
+        public const int PAPER = 2;
+
+        private string name;
+
+        private int winCount = 0;
+
+        /// <summary>
+        /// 名前
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+
+        public int ShowHand()
+        {
+            int playerHand = 0;
+
+            Random rnd = new Random();
+            double randomNum = rnd.Next(0, 300);
+            if (randomNum < 100)
+            {
+                playerHand = STONE;
+            }
+            else if (randomNum < 200)
+            {
+                playerHand = SCISSORS;
+            }
+            else if (randomNum < 300)
+            {
+                playerHand = PAPER;
+            }
+
+            return playerHand;
+        }
+
+        public void NotifyResult(bool result)
+        {
+            if (result)
+            {
+                winCount++;
+            }
+        }
+
+        public int GetWinCount()
+        {
+            return winCount;
+        }
+    }
+}
---
+++ Chapter3/Janken/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Janken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Judge saito = new Judge();
+
+            Player murata = new Player("村田さん");
+
+            Player yamada = new Player("山田さん");
+
+            saito.StartJanken(murata, yamada);
+        }
+    }
+}
